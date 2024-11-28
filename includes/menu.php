b4b80813a5df61db9b0837a0cb88e7b7e77cb001
Morais83 <60395329+Morais83@users.noreply.github.com>
2024-11-28 16:55:48 +0000

    Add files via upload

---
+++ includes/menu.php
@@ -0,0 +1,12 @@
+<!-- Menu principal -->
+<link rel="stylesheet" href="css/style.css">
+<nav class="navbar">
+            <ul class="navbar-menu">
+                <li><a href="index.php" class="nav-link active">Home</a></li>
+                <li><a href="preco.php" class="nav-link">Preçário</a></li>
+                <li><a href="sobre.php" class="nav-link">Sobre Nós</a></li>
+                <li><a href="mobile.php" class="nav-link">Mobile Homes</a></li>
+                <li><a href="tendas.php" class="nav-link">Tendas/Caravanas</a></li>
+                <li><a href="galeria.php" class="nav-link">Galeria</a></li>
+            </ul>
+        </nav>
